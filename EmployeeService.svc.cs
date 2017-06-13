e8d8e647a7c55268a6f2fd40684bfa77fb513965
Kayini <kayini88@gmail.com>
2017-06-13 10:16:57 +0530

    Add files via upload

---
+++ EmployeeService.svc.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace EmplyeeServiceProject
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "EmployeeService" in code, svc and config file together.
+    public class EmployeeService : IEmployeeService
+    {
+
+        static private string sqlConString
+        {
+            get { return ConfigurationManager.ConnectionStrings["SQLConnectionString"].ConnectionString; }
+        }
+        SqlConnection conn = new SqlConnection(sqlConString);
+
+       
+        public List<ArchieveData> GetArchieveLogsList()
+        {
+
+            List<ArchieveData> archieveLogs = new List<ArchieveData>();
+
+            using (SqlConnection con = new SqlConnection("Database Connection String"))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT [logs],[fromDate],[toDate] FROM [dbo].[Logs]", con))
+                {
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    while (dr.Read())
+                    {
+                        ArchieveData logs = new ArchieveData();
+                        logs.Logs = dr[0].ToString();
+                        logs.FromDate = Convert.ToDateTime(dr[1].ToString());
+                        logs.ToDate = Convert.ToDateTime(dr[2].ToString());
+                        archieveLogs.Add(logs);
+                    }
+                }
+            }
+
+            return archieveLogs;
+        }
+    }
+}
+  
+            
+           
+
+
---
+++ EmployeeTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ServiceModel;
+
+
+namespace EmployeeServiceTest
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class EmployeeTest
+    {
+        private EmployeeService.EmployeeServiceClient client;
+        
+        [TestMethod]
+        public void TestGetArchieveLogsList1()
+        {
+            List<EmployeeService.ArchieveData> lstLogs;
+            EmployeeService.IEmployeeService emp;
+
+            using (EmployeeService.EmployeeServiceClient empService = new EmployeeService.EmployeeServiceClient())
+            {
+                lstLogs = empService.GetArchieveLogsList().ToList();
+            }
+            Assert.IsTrue(lstLogs.Count>0);
+        }
+
+        [TestMethod]
+        public void TestGetArchieveLogsList2()
+        {
+            List<EmployeeService.ArchieveData> lstLogs;
+            EmployeeService.IEmployeeService emp;
+
+            using (EmployeeService.EmployeeServiceClient empService = new EmployeeService.EmployeeServiceClient())
+            {
+                lstLogs = empService.GetArchieveLogsList().ToList();
+            }
+            Assert.IsNull(lstLogs);
+        }
+
+        [TestMethod]
+        public void TestGetArchieveLogsList3()
+        {
+            List<EmployeeService.ArchieveData> lstLogs;
+            EmployeeService.IEmployeeService emp;
+
+            using (EmployeeService.EmployeeServiceClient empService = new EmployeeService.EmployeeServiceClient())
+            {
+                lstLogs = empService.GetArchieveLogsList().ToList();
+            }
+            Assert.Equals(lstLogs.Count,3);
+        }
+
+        [TestMethod]
+        public void TestGetArchieveLogsList4()
+        {
+            List<EmployeeService.ArchieveData> lstLogs;
+            List<EmployeeService.ArchieveData> lstLogsContainer;
+            EmployeeService.IEmployeeService emp;
+
+            Mock<emp> mockContainer = new Mock<emp>();
+            lstLogsContainer = mockContainer.GetArchieveLogsList();
+            using (EmployeeService.EmployeeServiceClient empService = new EmployeeService.EmployeeServiceClient())
+            {
+                lstLogs = empService.GetArchieveLogsList().ToList();
+            }
+            Assert.Equals(lstLogs.Count, lstLogsContainer.Count);
+        }
+
+        
+    }
+}
